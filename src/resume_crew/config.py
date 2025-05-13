c3890056282bdaf90f9a72e4cd5a51cd2b4830fe
Martin Helmhout <linuxonsulting2@gmail.com>
2025-05-13 09:41:32 +0200

    feat: Introduce configuration for job URL and company name
    - Added a new config.py file to store job-related constants.
    - Updated main.py to utilize the new configuration for job URL and company name.

---
+++ src/resume_crew/config.py
@@ -0,0 +1,2 @@
+JOB_URL='https://www.mckinsey.com/careers/search-jobs/jobs/associate-15178'
+COMPANY_NAME='Mckinsey & Co.'
--- src/resume_crew/main.py
+++ src/resume_crew/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import sys
 import warnings
-
+from config import JOB_URL, COMPANY_NAME
 from resume_crew.crew import ResumeCrew
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
@@ -11,8 +11,8 @@ def run():
     Run the resume optimization crew.
     """
     inputs = {
-        'job_url': 'https://www.mckinsey.com/careers/search-jobs/jobs/associate-15178',
-        'company_name': 'Mckinsey & Co.'
+        'job_url': JOB_URL,
+        'company_name': COMPANY_NAME
     }
     ResumeCrew().crew().kickoff(inputs=inputs)
 
